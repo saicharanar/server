58f700fc504bd8bb4c381f29a5cd0b8aad304057
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-06-29 10:21:07 +0530

    Introduced router and runServer

--- src/server.js
+++ src/server.js
@@ -1,17 +1,32 @@
 const http = require('http');
 const { URL } = require('url');
+const { createRouter } = require('./createRouter');
 
-const startServer = (port, handler) => {
+const runServer = (port, router) => {
   const server = http.createServer((request, response) => {
     const baseUrl = `http://${request.headers.host}`;
     request.url = new URL(request.url, baseUrl);
     console.log(request.method, request.url.pathname);
-    handler(request, response);
+    router(request, response);
   });
 
   server.listen(port, () => {
     console.log(`Server Bound to ${server.address().port}`);
   });
+};
+
+const startServer = (port, ...handlers) => {
+  const router = createRouter(handlers);
+  runServer(port, router);
+};
+
+const homeHandler = (request, response) => {
+  if (request.url.pathname === '/home') {
+    response.end('ok');
+    return true;
+  }
+
+  return false;
 };
 
 const notFoundHandler = (request, response) => {
@@ -20,4 +35,4 @@ const notFoundHandler = (request, response) => {
   return true;
 };
 
-startServer(9090, notFoundHandler);
+startServer(9090, homeHandler, notFoundHandler);
